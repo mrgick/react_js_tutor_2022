59681433c55b2d582d2c2bf296866fd1f8f768fd
mrgick <45306973+mrgick@users.noreply.github.com>
2022-02-25 14:51:20 +0300

    Updates

---
+++ precache-manifest.d710158103cd6a16ab0c7c1aacc48a54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "225d63c96e6dd78013ea",
+    "url": "/react_js_tutor_2022/static/css/main.b85251c4.chunk.css"
+  },
+  {
+    "revision": "225d63c96e6dd78013ea",
+    "url": "/react_js_tutor_2022/static/js/main.db77bcbd.chunk.js"
+  },
+  {
+    "revision": "3b141514df51d0cae4d7",
+    "url": "/react_js_tutor_2022/static/js/runtime~main.24c65600.js"
+  },
+  {
+    "revision": "2a9c2985a97200c96a5b",
+    "url": "/react_js_tutor_2022/static/css/2.b2a96ac0.chunk.css"
+  },
+  {
+    "revision": "2a9c2985a97200c96a5b",
+    "url": "/react_js_tutor_2022/static/js/2.a8390cfb.chunk.js"
+  },
+  {
+    "revision": "eb33792eb71720125abf",
+    "url": "/react_js_tutor_2022/static/js/3.495e12dd.chunk.js"
+  },
+  {
+    "revision": "fdb9692d63fef5515f613194efb62057",
+    "url": "/react_js_tutor_2022/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72aca9dcc2551a76eba1d3187d9fbd69.js"
+  "/react_js_tutor_2022/precache-manifest.d710158103cd6a16ab0c7c1aacc48a54.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react_js_tutor_2022/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
